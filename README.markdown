d64e3caf6bf3c8bc4a1205d7eacbb8b18b15302b
Meitar Moscovitz <meitarm@gmail.com>
2015-07-16 23:37:19 -0600

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,11 @@
+# Time waster check in - README
+
+This is a simple userscript that tries to help you avoid wasting time on websites that probably aren't the best use of your time. It's not a blocker, it is simply a small, invisible timer that will try to remind you that there are probably better uses of your time than repetetively reloading your Facebook newsfeed.
+
+# To do:
+
+* Add a feature that allows customization of which sites are "time wasters."
+
+# Inspiration
+
+Many [thanks to Rebecca and Brad for the inspiration](https://twitter.com/bradweikel/status/621415340388388865).
---
+++ timewastercheckin.user.js
@@ -0,0 +1,51 @@
+﻿/**
+ * This is a Greasemonkey script and must be run using a Greasemonkey-compatible browser.
+ *
+ * @author maymay <bitetheappleback@gmail.com>
+ */
+// ==UserScript==
+// @name           Time waster check-in
+// @version        0.0.1
+// @namespace      net.maymay.timewastercheckin
+// @updateURL      https://github.com/meitar/timewastercheckin/raw/master/timewastercheckin.user.js
+// @description    Discourages use of time-wasting sites with a "soft" blocker that checks in about whether you really want to be on this site.
+// @include        http://www.tumblr.com/*
+// @include        https://www.tumblr.com/*
+// @include        https://twitter.com/*
+// @include        https://www.facebook.com/*
+// @include        https://fetlife.com/*
+// @grant          GM_log
+// @grant          GM_addStyle
+// @grant          GM_getValue
+// @grant          GM_setValue
+// @grant          GM_deleteValue
+// ==/UserScript==
+
+// TODO:
+// * Add a white listing feature.
+// * Add a feature to allow customizations of which sites are "time wasters."
+
+CHECK_IN = {};
+CHECK_IN.config = {
+    'debug': true,
+    'check_in_interval': GM_getValue('check_in_interval', 3600000), // 1 hour by default
+    'now': new Date().getTime()
+};
+
+CHECK_IN.hostIsPermitted = function (host) {
+    var nag_time = GM_getValue(host + '_is_permitted', false);
+    return (!nag_time || CHECK_IN.config.now > nag_time) ? false : true;
+};
+
+CHECK_IN.main = function () {
+    if (!CHECK_IN.hostIsPermitted(window.location.host)) {
+        if (window.confirm('Are you sure you want to continue using ' + window.location.host + '? There are probably better uses of your time, right?')) {
+            GM_setValue(window.location.host + '_is_permitted', CHECK_IN.config.now + CHECK_IN.config.check_in_interval);
+            window.setTimeout(CHECK_IN.main, CHECK_IN.config.check_in_interval);
+        } else {
+            window.location.href = 'http://lmgtfy.com/?q=what+should+I+do+now';
+        }
+    }
+};
+
+window.addEventListener('DOMContentLoaded', CHECK_IN.main);
